63120be5601ae246c66fcc538b5d2e2cc5ee16da
Xavier Figueroa <xavierfigueroav@gmail.com>
2019-07-12 01:56:00 -0500

    Modify menu and comment box.
    Signed-off-by: Xavier Figueroa <xavierfigueroav@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var DATA;
 var app;
-var URL = "https://firebasestorage.googleapis.com/v0/b/governus-803f2.appspot.com/o/governus-803f2-export.json?alt=media&token=6962cc81-a7ea-41d5-a802-d35aea627880";
+var URL = "https://firebasestorage.googleapis.com/v0/b/governus-803f2.appspot.com/o/data.json?alt=media&token=8a5f6936-6306-4bfc-8b3d-a05fcd7696ae";
 
 function load_data(){
     $.get(URL, function(data){
--- js/single-page.js
+++ js/single-page.js
@@ -49,7 +49,7 @@ function comment(){
     comments_app.law_comments.push(tmp_comment);
 }
 
-var URL = "https://firebasestorage.googleapis.com/v0/b/governus-803f2.appspot.com/o/governus-803f2-export.json?alt=media&token=6962cc81-a7ea-41d5-a802-d35aea627880";
+var URL = "https://firebasestorage.googleapis.com/v0/b/governus-803f2.appspot.com/o/data.json?alt=media&token=8a5f6936-6306-4bfc-8b3d-a05fcd7696ae";
 
 function load_data(){
     $.get(URL, function(data){
@@ -78,7 +78,6 @@ function load_data(){
         comments_app = new Vue({
             el: '#comments-app',
             data: {
-                comments: comments_data,
                 law_comments : comments
             }
         })
